c9d14a55aa0dc3f05b455be6bc25ce7b13444187
Antti Saarinen <antti.p.saarinen@gmail.com>
2013-09-09 11:09:52 +0300

    added readme and validate

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 indoor-json
 ===========
+
+This document specifies a GeoJSON variant used by indoor.io toolset to define indoor maps. This format is called IndoorJSON.
+
+Included is also a JavaScript validator that can be used to validate a given file to be valid IndoorJSON.
+
+Features
+==
+
+IndoorJSON may consist of any number of Features and/or FeatureCollections. All Features are interpreted similarly regardless of their grouping into nested FeatureCollections.
+
+Geometry Types
+==
+
+IndoorJSON supports all GeoJSON geometry types.
+
+`Point` and geometry types are interpreted as general Points of Interest. `MultiPoint` geometry type equals multiple similar `Point` geometries.
+
+`LineString` and `MultiLineString` are interpreted as walls, doorways and level connectors.
+
+`Polygon` and `MultiPolygon` are interpreted as open areas, e.g. rooms, halls, corridors. Typically, `Polygon` boundaries match respective `LineString` geometries, but it is equally possible for a single polygon to cover a larger area. 
+
+`GeometryCollection` geometries are interpreted similarly as if included geometries had been specified outside of the collection.
+
+Please note that even if `Polygon` and `MultiPolygon` geometries typically designate rooms, there MAY be multiple such geometries overlapping each other.
+
+Please not that if `LineString` geometries intersect each other on a given level, they are interpreted as if there had been multiple similar `LineString`s joining at the intersection point. Therefore it is recommended that IndoorJSON files do not contain intersecting `LineString`s residing on the same level.
+
+Properties
+==
+
+Each IndoorJSON Feature MAY have the following properties:
+
+- `level`: an integer designating the floor number of the Feature. Typically it is assumed that `0` means ground level. If not present, defaults to `0`.
+
+- `geomType`: an object containing additional information for the visualization of the geometry. This could be for instance specifying whether the geometry is a pillar (for `Point` geometries), a wall, doorway, window or railing (for `LineString` geometries), or a regular floor, blocked area or hole (for `Polygon` geometries), or a elevator, stairway or escalator (for `LineString` geometries with `connector` property).
+
+Features with `LineString` or `MultiLineString` geometry type may have the following properties:
+
+- `accessible`: a boolean value indicating whether the line is accessible e.g. if it can be walked through. If not present, defaults to `false`.
+
+Features with `LineString` geometry type may have the following properties:
+
+- `connector`: a boolean value indicating whether this line designates a connecting line between floors. 
+
+- `direction`: an integer designating the direction that this connector may be used for. `0` means both ways, `1` means only up, `2` means only down.
+
+For `connector` features, the `level` property is interpreted as the level of the level above. The level below is interpreted to be `level - 1`. If a `connector` feature has a `level` property that equals the lowest level in the file, that connector is not accessible.
+
+Validation
+==
+
+All valid GeoJSON files are also valid IndoorJSON files. 
+
+This project includes a simple validator that can be run using Node.js:
+
+```
+$ node validate.js indoor-example.json
+```
---
+++ validate.js
@@ -0,0 +1,242 @@
+
+if( typeof process != 'undefined' ) {
+    if( typeof process.argv[2] != 'string' ) {
+	process.stderr.write('usage: node validate.js [indoorjson-file]\n');
+	process.exit(1);
+	return;
+    }
+
+    try {
+	var json = JSON.parse(require('fs').readFileSync
+			      (process.argv[2]).toString('utf8'));
+    } catch(err) {
+	process.stderr.write('invalid JSON at ' + process.argv[2]);
+	process.exit(1);
+	return;
+    }
+
+    var res = validateGeoJSON(json);
+    if( res ) {
+	process.stderr.write('invalid GeoJSON: ' + res + '\n');
+	process.exit(1);
+	return;
+    } else 
+	process.exit(0);
+}
+
+function validateCoords(c) {
+    var res = validateArray(c);
+    if( res )
+	return res;
+    if( c.length < 2 )
+	return 'not enough coordinates';
+    if( typeof c[0] != 'number' ||
+	typeof c[1] != 'number' )
+	return 'non-number coordinates';
+    if( isNaN(c[0]) || isNaN(c[1]) )
+	return 'NaN coordinates';
+    return null;
+}
+
+function validateArray(arr) {
+    if( arr == null )
+	return 'null array';
+    else if( typeof arr != 'object' )
+	return 'non-object array';
+    else if( typeof arr.length != 'number' )
+	return 'unknown array length';
+    else if( Math.floor(arr.length) != arr.length )
+	return 'non-integer geometries array length';
+    else {
+	for( var ai = 0; ai < arr.length; ai++ ) 
+	    if( typeof arr[ai] == 'undefined' )
+		return 'undefined array element';
+	for( var a in arr ) { 
+	    if( a == 'length' )
+		continue;
+	    if( a.match(/^[0-9]+$/) ) {
+		var aint = parseInt(a);
+		if( a < 0 || a >= arr.length )
+		    return 'unknown attribute in array: ' + a;
+	    } else
+		return 'unknown attribute in array: ' + a;
+	}
+    }
+    return null;
+}
+
+function validateGeometry(geom, toplevel) {
+    if( geom == null )
+	return 'null geometry';
+    else if( typeof geom != 'object' )
+	return 'non-object geometry';
+    else if( typeof geom.type != 'string' )
+	return 'no geometry type';
+    else if( geom.type == 'GeometryCollection' ) {
+	var res = validateArray(geom.geometries);
+	if( res )
+	    return res;
+
+	for( var gi = 0; gi < geom.geometries.length; gi++ ) {
+	    var res = validateGeometry(geom.geometries[gi]);
+	    if( res )
+		return res;
+	}
+
+	for( var a in geom ) {
+	    if( a == 'crs' && toplevel ) {
+		var res = validateCRS(feat.crs);
+		if( res )
+		    return res;
+		continue;
+	    }
+	    if( a != 'type' &&
+		a != 'geometries' )
+		return 'unknown attribute ' + a;
+	}
+	return null;
+    } else { 
+	var res = validateArray(geom.coordinates);
+	if( res )
+	    return res;
+	if( geom.type == 'Point' ) {
+	    var res = validateCoords(geom.coordinates);
+	    if( res ) 
+		return res;
+	} else if( geom.type != 'MultiPoint' ||
+		   geom.type != 'LineString' ) {
+	    for( var gi = 0; gi < geom.coordinates.length; gi++ ) {
+		var res = validateCoords(geom.coordinates[gi]);
+		if( res )
+		    return res;
+	    }
+	} else if( geom.type != 'MultiLineString' ||
+		   geom.type != 'Polygon' ) {
+	    for( var pi = 0; pi < geom.coordinates.length; pi++ ) {
+		var res = validateArray(geom.coordinates[pi]);
+		if( res )
+		    return res;
+		for( var gi = 0; gi < geom.coordinates[pi].length; gi++ ) {
+		    var res = validateCoords(geom.coordinates[pi][gi]);
+		    if( res )
+			return res;
+		}	    
+	    }
+	} else if( geom.type != 'MultiPolygon' ) {
+	    for( var ppi = 0; ppi < geom.coordinates.length; ppi++ ) {
+		var res = validateArray(geom.coordinates[ppi]);
+		if( res )
+		    return res;
+		for( var pi = 0; pi < geom.coordinates[ppi].length; pi++ ) {
+		    var res = validateArray(geom.coordinates[ppi][pi]);
+		    if( res )
+			return res;
+		    for( var gi = 0; gi < geom.coordinates[ppi][pi].length; 
+			 gi++ ) {
+			var res = validateCoords(geom.coordinates[ppi][pi][gi]);
+			if( res )
+			    return res;
+		    }	    
+		}
+	    }	    
+	} else
+	    return 'unknown geometry type';
+	for( var a in geom ) {
+	    if( a == 'crs' && toplevel ) {
+		var res = validateCRS(feat.crs);
+		if( res )
+		    return res;
+		continue;
+	    }
+	    if( a != 'type' &&
+		a != 'coordinates' )
+		return 'unknown attribute ' + a;
+	}
+	return null;
+    }
+}
+
+function validateCRS(crs) {
+    if( crs == null )
+	return 'null crs';
+    else if( typeof crs != 'object' ) 
+	return 'non-object crs';
+    else if( typeof crs.type != 'string' )
+	return 'non-string crs type';
+    else if( typeof crs.properties != 'object' )
+	return 'non-object crs properties';
+    for( var a in crs )
+	if( a != 'type' &&
+	    a != 'properties' )
+	    return 'unknown attribute in crs: ' + a;
+    return null;
+}
+
+function validateFeature(feat, toplevel) {
+    if( feat == null )
+	return 'null feature';
+    else if( typeof feat != 'object' )
+	return 'non-object feature';
+    else if( typeof feat.type == 'string' ) {
+	if( feat.type == 'Feature' ) {
+
+	    var res = validateGeometry(feat.geometry, false);
+	    if( res )
+		return res;
+	    
+	    if( typeof feat.properties != 'object' )
+		return 'no properties in a feature';
+
+	    for( var a in feat ) {
+		if( a != 'type' &&
+		    a != 'geometry' &&
+		    a != 'properties' )
+		    return 'unknown attribute ' + a;
+	    }
+	} else if( feat.type == 'FeatureCollection' ) {
+	    var res = validateArray(feat.features);
+	    if( res )
+		return res;
+	    for( var fi = 0; fi < feat.features.length; fi++ ) {
+		var res = validateFeature(feat.features[fi], false);
+		if( res )
+		    return res;
+	    }
+	    for( var a in feat ) {
+		if( a == 'crs' && toplevel ) {
+		    var res = validateCRS(feat.crs);
+		    if( res )
+			return res;
+		    continue;
+		}
+		if(a != 'type' &&
+		   a != 'features' ) 
+		    return 'unknown attribute ' + a;
+	    }
+	    return null;
+	} else
+	    return 'invalid feature type ' + feat.type;
+    } else
+	return 'invalid feature ' + JSON.stringify(feat).substring(0, 50);
+}
+
+function validateObject(obj, toplevel) {
+    if( obj == null )
+	return 'null object';
+    if( typeof obj != 'object' )
+	return 'non-object object';
+    if( typeof obj.type != 'string' )
+	return 'unknown type';
+    if( obj.type == 'Feature' ||
+	obj.type == 'FeatureCollection' )
+	return validateFeature(obj, toplevel);
+    else 
+	return validateGeometry(obj, toplevel);
+}
+
+function validateGeoJSON(json) {
+    return validateFeature(json, true);
+}
+
+
+
